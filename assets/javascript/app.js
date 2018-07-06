a621afe7abcd7895aa9af57e076c454294ee3af9
Ednas <edna@edna.tech>
2018-07-05 23:19:27 -0400

    Working on sort function and cleaned up old CLogs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,83 +1,72 @@
 // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyCyFwNZjys0krmoC3U01ZYvYKzQ17_3BS8",
     authDomain: "train-time-2c1cc.firebaseapp.com",
     databaseURL: "https://train-time-2c1cc.firebaseio.com",
     projectId: "train-time-2c1cc",
     storageBucket: "",
     messagingSenderId: "591191230804"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 var trainData = firebase.database().ref();
 
 
 // Button for adding trains
-$("#addTrainBtn").on("click", function(){
-event.preventDefault();  // Keeps from refreshing the page
-	// Grabs user input
-	var trainName = $("#trainNameInput").val().trim();
-	var destination = $("#destinationInput").val().trim();
-	var firstTrainUnix = moment($("#firstTrainInput").val().trim(), "HH:mm").subtract(10, "years").format("X");
-	var frequency = $("#frequencyInput").val().trim();
+$("#addTrainBtn").on("click", function() {
+    event.preventDefault();
+    // Grabs user input
+    var trainName = $("#trainNameInput").val().trim();
+    var destination = $("#destinationInput").val().trim();
+    var firstTrain = moment($("#firstTrainInput").val().trim(), "HH:mm").subtract(10, "years").format("X");
+    var frequency = $("#frequencyInput").val().trim();
 
-	// Creates local "temporary" object for holding train data
-	var newTrain = {
-		name:  trainName,
-		destination: destination,
-		firstTrain: firstTrainUnix,
-		frequency: frequency
-	}
+    // Creates local "temporary" object for holding train data
+    var newTrain = {
+        name: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+    }
 
-	// Uploads train data to the database
-	trainData.push(newTrain);
+    // Uploads train data to the database
+    trainData.push(newTrain);
 
-	// Logs everything to console
-	// console.log(newTrain.name);
-	// console.log(newTrain.destination); 
-	// console.log(firstTrainUnix);
-	// console.log(newTrain.frequency)
+    // Alert
+    alert(newTrain.name + " has been successfully added");
 
-	// Alert
-	alert(newTrain.name + " has been successfully added");
+    // Clears all of the text-boxes
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#firstTrainInput").val("");
+    $("#frequencyInput").val("");
 
-	// Clears all of the text-boxes
-	$("#trainNameInput").val("");
-	$("#destinationInput").val("");
-	$("#firstTrainInput").val("");
-	$("#frequencyInput").val("");
-
-	return false;
+    return false;
 });
 
 
 // Create Firebase event for adding trains to the database and a row in the html when a user adds an entry
-trainData.on("child_added", function(childSnapshot, prevChildKey){
+trainData.on("child_added", function(childSnapshot) {
 
-	// Store everything into a variable.
-	let data = childSnapshot.val();
-	let tName = data.name;
-	let tDestination = data.destination;
-	let tFrequency = data.frequency;
-	let tFirstTrain = data.firstTrain;
-	// Calculate the minutes until arrival using hardcore math
-	// To calculate the minutes till arrival, take the current time in unix subtract the FirstTrain time and find the modulus between the difference and the frequency  
-	let differenceTimes = moment().diff(moment.unix(tFirstTrain), "minutes");
-	let tRemainder = moment().diff(moment.unix(tFirstTrain), "minutes") % tFrequency ;
-	let tMinutes = tFrequency - tRemainder;
+    let data = childSnapshot.val();
+    let trainNames = data.name;
+    let trainDestin = data.destination;
+    let trainFrequency = data.frequency;
+    let theFirstTrain = data.firstTrain;
 
-	// To calculate the arrival time, add the tMinutes to the currrent time
-	let tArrival = moment().add(tMinutes, "m").format("hh:mm A"); 
-	// console.log(tMinutes + " minutes to arrival");
-	// console.log(tArrival + " Arrival time");
-	// console.log(moment().format("hh:mm A") + " current time Am/Pm format");
-	// console.log(moment().format("X") + " current time in Unix format");
+    // Calculate the minutes until arrival using hardcore math
+    // To calculate the minutes till arrival, take the current time in unix subtract the FirstTrain time and find the modulus between the difference and the frequency  
+    let tRemainder = moment().diff(moment.unix(theFirstTrain), "minutes") % trainFrequency;
+    let tMinutes = trainFrequency - tRemainder;
 
-	// [Work on later] Sort function
-	// let nextTrain = data.firstTrain.orderByValue();
-	// console.log(nextTrain);
-	// Add each train's data into the table 
-	$("#trainTable > tbody").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td id='min'>" + tFrequency + "</td><td id='min'>" + tArrival + "</td><td id='min'>" + tMinutes + "</td></tr>");
+    // To calculate the arrival time, add the tMinutes to the currrent time
+    let tArrival = moment().add(tMinutes, "m").format("hh:mm A");
+
+    // [Work on later] Sort function
+    // data.ref.orderByChild("firstTrain")
+    // console.log(nextTrain);
+    // Add each train's data into the table 
+    $("#trainTable > tbody").append("<tr><td>" + trainNames + "</td><td>" + trainDestin + "</td><td id='min'>" + trainFrequency + "</td><td id='min'>" + tArrival + "</td><td id='min'>" + tMinutes + "</td></tr>");
 
 });
 
@@ -95,37 +84,3 @@ $("#currentTime").append(moment().format("hh:mm A"));
 // });
 // var points = [40, 100, 1, 5, 25, 10];
 // points.sort(function(a, b){return a - b});
-
-
-// Assume the following situations. 
-
-// (TEST 1) 
-// First Train of the Day is 3:00 AM 
-// Assume Train comes every 3 minutes.
-// Assume the current time is 3:16 AM....
-// What time would the next train be...? (Use your brain first)
-// It would be 3:18 -- 2 minutes away
-
-// (TEST 2) 
-// First Train of the Day is 3:00 AM 
-// Assume Train comes every 7 minutes.
-// Assume the current time is 3:16 AM....
-// What time would the next train be...? (Use your brain first)
-// It would be 3:21 -- 5 minutes away
-
-// ==========================================================
-
-// Solved Mathematically
-// Test case 1: 
-// 16 - 00 = 16
-// 16 % 3 = 1 (Modulus is the remainder)
-// 3 - 1 = 2 minutes away
-// 2 + 3:16 = 3:18
-
-// Solved Mathematically  
-// Test case 2: 
-// 16 - 00 = 16
-// 16 % 7 = 2 (Modulus is the remainder)
-// 7 - 2 = 5 minutes away
-// 5 + 3:16 = 3:21
-
